26a5c955edc4a6996e9f055a6e618f2ae162cb0b
Andy <shaofa@gmail.com>
2026-07-14 22:56:51 +0900

    JEC on

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'kjw4cH60N0qv5_z50fTTY6iVvK2ZXrZy',
+            'X-App-Proxy': 'IEetpAT7oOV8ly2ZNHzzP3mBZxOdiU7x',
           },
           body: JSON.stringify(requestDetails),
         };
