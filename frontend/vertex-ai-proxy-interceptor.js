076080e772689f3df6b9bf51e77f611d336fc13d
Andy <shaofa@gmail.com>
2026-07-15 09:08:58 +0900

    qr-code

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'IEetpAT7oOV8ly2ZNHzzP3mBZxOdiU7x',
+            'X-App-Proxy': 'oF9DWtR3eemQ0jZViNQC_EKBkMaZNZRA',
           },
           body: JSON.stringify(requestDetails),
         };
